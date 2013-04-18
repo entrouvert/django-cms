e0b6f3f94c6ae8c1083e7eb8160cbaff1a141ea1
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2013-04-18 20:52:02 +0200

    compat: add method to retrieve the user_set of a group

--- cms/compat.py
+++ cms/compat.py
@@ -5,6 +5,11 @@ __all__ = ['User']
 if django.VERSION >= (1, 5):
     from django.contrib.auth import get_user_model
     User = get_user_model()
+    def get_user_set(group):
+        related_query_name = User.groups.field.related_query_name
+        return getattr(group, '%s_set' % related_query_name)
 else:
     from django.contrib.auth.models import User
     get_user_model = lambda:User
+    def get_user_set(group):
+        return group.user_set
